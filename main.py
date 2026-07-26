800ddb9271c9c0def2cad4eee8ff9050b72725fa
AndriiHubenok <andrewhu08@gmail.com>
2026-07-26 14:03:50 +0300

    add roundtrip command

--- main.py
+++ main.py
@@ -6,31 +6,26 @@ for raw in sys.stdin:
     if not line: print(""); continue
     parts = line.split(" ", 1)
 
-    if len(parts) == 1:
-        data = parts[0]
-
-        try:
-            bytes.fromhex(data)
-            print(custom_base64.encode_bytes(data))
-
-        except ValueError:
-            print(custom_base64.decode_str_to_hex(data))
-        continue
-
     cmd = parts[0]
     arg = parts[1] if len(parts) > 1 else ""
+
     if cmd == "ENCODE":
-        print(custom_base64.encode(arg))
+        print(custom_base64.encode_bytes(arg))
 
     elif cmd == "DECODE":
-        try: print(custom_base64.decode(arg))
-        except Exception: print("INVALID")
+        print(custom_base64.decode_str_to_hex(arg))
 
     elif cmd == "ENCODE_URL":
         print(custom_base64.encode_url(arg))
 
     elif cmd == "DECODE_URL":
         print(custom_base64.decode_url(arg))
+
+    elif cmd == "ROUNDTRIP":
+        if custom_base64.decode_str_to_hex(custom_base64.encode_bytes(arg)) == arg:
+            print("OK")
+        else:
+            print("FAIL")
 
     elif cmd == "VAL":
         try: print(custom_base64.get_value(arg))
